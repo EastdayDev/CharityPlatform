4d0dba82a2c5a7f5b272111544a9fdf0ce7cf2d4
lcsoftware <lcsoftware@126.com>
2015-07-07 16:23:44 +0800

    auto

---
+++ CharityPlatformWeb/app/project/controllers/project.edit.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('projectModule').controller('ProjectEditController',
+['$scope', '$state', '_project', '$rootScope', 'epModal',
+function ($scope, $state, _project, $rootScope, epModal) {
+
+            
+
+
+
+        }]);
--- CharityPlatformWeb/app/project/project.routes.js
+++ CharityPlatformWeb/app/project/project.routes.js
@@ -3,24 +3,24 @@
 angular.module('projectModule').config(['$stateProvider', function ($stateProvider) {
     ///我的项目
    $stateProvider
-        .state('projectMine', {
-            url: '/project/mine',
+        .state('user.projectMine', {
+            url: '/user/project',
             templateUrl: '/app/project/views/project.mine.html',
             controller: 'ProjectMineController'
-        }) 
+        })
         .state('projectEdit', {
             url: '/project/edit/:id',
             templateUrl: '/app/project/views/project.edit.html',
             controller: 'ProjectEditController'
-        }) 
+        })
         .state('projectList', {
             url: '/project/list/:id',
             templateUrl: '/app/project/views/project.List.html',
             controller: 'ProjectListController'
-        }) 
+        })
         .state('projectView', {
             url: '/project/view/:id',
             templateUrl: '/app/project/views/project.view.html',
             controller: 'ProjectViewController'
-        }) 
+        })
 }]);
