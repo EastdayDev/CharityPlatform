3555469ebcedd7bfc5ee00f984e6141b670f3123
lcsoftware <lcsoftware@126.com>
2015-07-15 10:55:52 +0800

    auto

--- CharityPlatformWeb/app/home/controllers/home.index.controller.js
+++ CharityPlatformWeb/app/home/controllers/home.index.controller.js
@@ -1,11 +1,33 @@
 'use strict';
 
-angular.module('homeModule').controller('HomeController',
-['$scope', function ($scope) {
-	var slides = $scope.slides = [];
-    // 添加轮播图源
-    slides.push({ image: '/images/rollpic_1.png', text: '' });
-    slides.push({ image: '/images/rollpic_2.png', text: '' });
-    slides.push({ image: '/images/rollpic_3.png', text: '' });
-}]);
-    
+angular.module('homeModule').controller('HomeController', ['$scope',
+	'_donation', '_user',
+	function(
+		$scope, _donation, _user) {
+		var slides = $scope.slides = [];
+		// 添加轮播图源
+		slides.push({
+			image: '/images/rollpic_1.png',
+			text: ''
+		});
+		slides.push({
+			image: '/images/rollpic_2.png',
+			text: ''
+		});
+		slides.push({
+			image: '/images/rollpic_3.png',
+			text: ''
+		});
+
+		$scope.$on('$viewContentLoaded', function() {
+			_donation.USP_Donation_Projects(_user.userId, function(data) {
+				if (data.length > 3) {
+					data.splice(3)
+				}
+				$scope.projects = data;
+
+			});
+		});
+
+	}
+]);
--- CharityPlatformWeb/app/user/controllers/user.login.controller.js
+++ CharityPlatformWeb/app/user/controllers/user.login.controller.js
@@ -1,27 +1,27 @@
 'use strict';
 
-angular.module('userModule').controller('UserLoginController',
-['$scope', '_user', '_app', '$state', 'epModal', 
-function ($scope, _user, _app, $state, epModal) { 
-	
-	$scope.userName = '456';
-	$scope.password = '123';
+angular.module('userModule').controller('UserLoginController', ['$scope',
+	'_user', '_app', '$state', 'epModal',
+	function($scope, _user, _app, $state, epModal) {
 
-	$scope.login = function(userName, password){
-		_user.login(userName, password, function(data){
-			if (data){				
-				_app.user = _user.user;
-				if (_app.user.I_Category === 101){
-					/// 基金会用户 
-					$state.go('sys.user');			
+		$scope.userName = 'JG01';
+		$scope.password = '123';
+
+		$scope.login = function(userName, password) {
+			_user.login(userName, password, function(data) {
+				if (data) {
+					_app.user = _user.user;
+					if (_app.user.I_Category === 101) {
+						/// 基金会用户
+						$state.go('sys.user');
+					} else {
+						var stateName = $scope.fromState.name || 'home';
+						$state.go(stateName);
+					}
 				} else {
-					var stateName = $scope.fromState.name || 'home'; 
-					$state.go(stateName);			
+					epModal.info('用户名密码错误！');
 				}
-			} else {
-				epModal.info('用户名密码错误！');
-			}
-		});
-	} 
-}]);
-    
+			});
+		}
+	}
+]);
